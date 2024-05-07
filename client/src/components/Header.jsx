76844feee5a5d576190f279cd8c36c0ce12b7086
Sarvesh Babu T Y <119612108+sarv05@users.noreply.github.com>
2024-05-07 18:48:14 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import './Navbar.css'; // import CSS file for styling
+import './Header.css'; // import CSS file for styling
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Header() {
   return (
     <div className="nav">
       <div className="nav-logo">
+        <Link to='/'>
         <p>Thyroid Classification App</p>
+        </Link>
       </div>
       <ul className="nav-menu">
+        <Link to='/'>
         <li>Home</li>
+        </Link>
+        <Link to='/about'>
         <li>About</li>
+        </Link>
       </ul>
     </div>
   );
 }
 
-export default Navbar;
+export default Header;
