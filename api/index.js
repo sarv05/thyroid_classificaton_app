4f8c192b1d1b47df847e861877c16a89f240bc82
Sarvesh Babu T Y <119612108+sarv05@users.noreply.github.com>
2024-05-07 20:04:28 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose.connect("mongodb+srv://sarveshdoveton2004:sarveshbabu@mern.ajzmmmx.mongodb.net/mern-thyroid?retryWrites=true&w=majority").then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const app = express();
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
